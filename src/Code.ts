64e9c79970f31635a77ce7193b9fb3d4a3dc0c36
kadoyau <el.psi.congroo@gmail.com>
2019-11-19 02:27:45 +0900

    メロンブックスのスクリプトも実行するようにする

--- src/Code.ts
+++ src/Code.ts
@@ -1,6 +1,9 @@
+import MelonbooksChecklistGenerator from "./MelonbooksChecklistGenerator";
 import ToranoanaChecklistGenerator from "./ToranoanaChecklistGenerator";
 
 function main() {
     const checklistGenerator = new ToranoanaChecklistGenerator();
     checklistGenerator.saveProductsFromTranoanaPromotionMail();
+    const melonbooksChecklistGenerator = new MelonbooksChecklistGenerator();
+    melonbooksChecklistGenerator.saveProductsFromPromotionMail();
 }
